1e1ecadb12352589cb502aeeaf6bcc623cb68919
fadhel <fadhelsitakka445@gmail.com>
2024-07-10 16:34:28 +0700

    fitur baru

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,67 @@ import "@/styles/tailwind.css";
 import { Providers } from "./providers";
 import { cx } from "@/utils/all";
 import { Inter, Lora } from "next/font/google";
+import Head from "next/head";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    default: "Padukuhan ngepoh",
+    template: "%s - Padukuhan ngepoh"
+  },
+  description: "Situs Resmi Padukuhan ngepoh.",
+  metadataBase: new URL("https://www.padukuhanngepoh.my.id"),
+  keywords: [
+    "Padukuhan ngepoh",
+    "ngepoh",
+    "ngepoh Gunungkidul",
+    "ngepoh Gunung Kidul",
+    "Padukuhan ngepoh Gunungkidul",
+    "Padukuhan ngepoh Gunung Kidul",
+    "Kerupuk Udang",
+    "Kerupuk Udang ngepoh",
+    "Kerupuk Udang ngepoh Gunungkidul",
+    "Kerupuk Udang ngepoh Gunung Kidul"
+  ],
+  robots: {
+    index: true,
+    follow: true
+  },
+  openGraph: {
+    title: "Padukuhan ngepoh",
+    description: "Situs Resmi Padukuhan ngepoh",
+    url: "https://www.padukuhanngepoh.my.id",
+    type: "website",
+    locale: "id_ID",
+    siteName: "Padukuhan ngepoh",
+    images: [
+      {
+        url: "https://www.padukuhanngepoh.my.id/img/ngepoh.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Padukuhan ngepoh"
+      }
+    ]
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Padukuhan ngepoh",
+    description: "Situs Resmi Padukuhan ngepoh",
+    images: [
+      {
+        url: "https://www.padukuhanngepoh.my.id/img/ngepoh.jpg",
+        alt: "Padukuhan ngepoh"
+      }
+    ]
+  },
+  alternates: {
+    canonical: "https://www.padukuhanngepoh.my.id",
+    languages: {
+      "id-ID": "https://www.padukuhanngepoh.my.id",
+      "en-US": "https://www.padukuhanngepoh.my.id/en"
+    }
+  }
+};
 
 const inter = Inter({
   subsets: ["latin"],
@@ -13,17 +74,45 @@ const lora = Lora({
   variable: "--font-lora"
 });
 
-export default function RootLayout({
-  children
-}: {
+interface RootLayoutProps {
   children: React.ReactNode;
-}) {
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html
-      lang="en"
+      lang="id"
       suppressHydrationWarning
       className={cx(inter.variable, lora.variable)}>
-      <body className="antialiased text-gray-800 dark:bg-black dark:text-gray-400">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        />
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      </Head>
+      <body className="text-gray-800 antialiased dark:bg-black dark:text-gray-400">
         <Providers>{children}</Providers>
       </body>
     </html>
---
+++ app/sitemap.ts
@@ -0,0 +1,26 @@
+// import { MetadataRoute } from "next";
+
+// export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+//   return [
+//     {
+//       url: "https://www.padukuhanngepoh.my.id",
+//       lastModified: new Date(),
+//     },
+//     {
+//       url: "https://www.padukuhanngepoh.my.id/Berita",
+//       lastModified: new Date(),
+//     },
+//     {
+//       url: "https://www.padukuhanngepoh.my.id/Kontak",
+//       lastModified: new Date(),
+//     },
+//     {
+//       url: "https://www.padukuhanngepoh.my.id/UMKM",
+//       lastModified: new Date(),
+//     },
+//     {
+//       url: "https://www.padukuhanngepoh.my.id/Kesenian",
+//       lastModified: new Date(),
+//     }
+//   ];
+// }
